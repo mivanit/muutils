4730725ea4d2f693de2c4024061c2a0ec68df60a
mivanit <mivanits@umich.edu>
2022-08-23 21:21:30 -0700

    wip

---
+++ muutils/defaulterdict.py
@@ -0,0 +1,18 @@
+from typing import Any, Callable, Generic, TypeVar
+
+from collections import defaultdict
+
+_KT = TypeVar('_KT'); _VT = TypeVar('_VT')
+class DefaulterDict(dict[_KT, _VT], Generic[_KT, _VT]):
+	"""like a defaultdict, but default_factory is passed the key as an argument"""
+	def __init__(self, default_factory: Callable[[_KT], _VT], *args, **kwargs):
+		if args:
+			raise TypeError(f'DefaulterDict does not support positional arguments: *args = {args}')
+		super().__init__(**kwargs)
+		self.default_factory: Callable[[_KT], _VT] = default_factory
+	
+	def __getitem__(self, k: _KT) -> _VT:
+		if k in self:
+			return dict.__getitem__(self, k)
+		else:
+			return self.default_factory(k)
--- muutils/tensor_utils.py
+++ muutils/tensor_utils.py
@@ -1,5 +1,6 @@
 import json
 import typing
+
 import torch
 
 # pylint: disable=protected-access
@@ -22,6 +23,16 @@ class ATensor(torch.Tensor):
         else:
             return typing._AnnotatedAlias(torch.Tensor, params)
 
+DTYPE_MAP: dict[str, torch.dtype] = {
+    str(x) : x
+    for x in [
+        torch.float, torch.float32, torch.float64, torch.half, torch.bfloat16, 
+        torch.complex64, torch.complex128,
+        torch.int, torch.int8, torch.int16, torch.int32, torch.int64, torch.long, torch.short,
+        torch.uint8, torch.bool,
+    ]
+}
+
 
 def lpad_tensor(tensor: torch.Tensor, padded_length: int, pad_value: float = 0.0) -> torch.Tensor:
 	"""pad a 1-d tensor on the left with pad_value to length `padded_length`"""
@@ -38,3 +49,31 @@ def rpad_tensor(tensor: torch.Tensor, pad_length: int, pad_value: float = 0.0) -
 	])
 
 
+
+def split_sequences(
+		sequences: typing.Iterator[ATensor["token"]],
+		min_length: int = 1,
+		max_length: int|None = None,
+        lpad_to: int|None = None,
+	) -> typing.Iterator[ATensor["token"]]:
+    """split a list of sequences into a list of sequences with length in [min_length, max_length]
+    
+    mostly for feeding data into transformers"""
+
+    if max_length is None:
+        max_length = max(len(seq) for seq in sequences)
+
+    for seq in sequences:
+        for i in range(len(seq)):
+            if i < min_length:
+                continue
+            
+            start_idx: int = max(0, i - max_length)
+            
+            if lpad_to is None:
+                yield seq[start_idx:i+1]
+            else:
+                yield torch.cat([
+                    torch.full((padded_length - tensor.shape[0],), pad_value, dtype=tensor.dtype, device=tensor.device),
+                    tensor,
+                ])

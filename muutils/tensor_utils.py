f382ecefd9142bd1975095fd7aac5244f1130680
mivanit <mivanits@umich.edu>
2023-02-13 19:47:00 -0700

    updated tensor_utils to use jaxtyping backend

--- muutils/tensor_utils.py
+++ muutils/tensor_utils.py
@@ -1,56 +1,120 @@
 import sys
 import json
 import typing
+import warnings
 
-
+import jaxtyping
 import numpy as np
 import torch
 
-# pylint: disable=protected-access
+
+# pylint: disable=missing-class-docstring
 
 
-def annotated_array_factory(
-        array_type: typing.Literal[torch.Tensor, np.ndarray],
-        name: str = "ATensor",
+TYPE_TO_JAX_DTYPE: dict[type, type] = {
+    float : jaxtyping.Float,
+    int : jaxtyping.Int,
+    np.float32: jaxtyping.Float,
+    np.float64: jaxtyping.Float,
+    np.int32: jaxtyping.Int,
+    np.int64: jaxtyping.Int,
+    torch.float32: jaxtyping.Float,
+    torch.float64: jaxtyping.Float,
+    torch.int32: jaxtyping.Int,
+    torch.int64: jaxtyping.Int,
+    jaxtyping.Float: jaxtyping.Float,
+    jaxtyping.Int: jaxtyping.Int,
+}
+
+
+def jaxtype_factory(
+        name: str,
+        array_type: type,
+        default_jax_dtype: type = jaxtyping.Float,
+        legacy_mode: typing.Literal["error", "warn", "ignore"] = "warn",
     ) -> type:
 
-    class BaseTensor(array_type):
-        """tensor type with annotation for shape and dim names"""
+    class _BaseArray:
+        """jaxtyping shorthand
+        (backwards compatible with older versions of muutils.tensor_utils)
 
-        __slots__ = ()
+        default_jax_dtype = {default_jax_dtype}
+        array_type = {array_type}
+        """
 
         def __new__(cls, *args, **kwargs):
-            raise TypeError("Type ATensor cannot be instantiated.")
+            raise TypeError("Type FArray cannot be instantiated.")
 
         def __init_subclass__(cls, *args, **kwargs):
-            raise TypeError(f"Cannot subclass {cls.__module__}.Annotated")
+            raise TypeError(f"Cannot subclass {cls.__name__}")
+
+        @classmethod
+        def param_info(cls, params) -> str:
+            """useful for error printing"""
+            return str({
+                "cls.__name__": cls.__name__,
+                "cls.__doc__": cls.__doc__,
+                "params": params,
+                "type(params)": type(params),
+            })
+
 
         @typing._tp_cache
         def __class_getitem__(cls, params):
-            if isinstance(params, type):
-                return typing._AnnotatedAlias(array_type, {"type": params})
-            elif len(params) == 0:
-                return array_type
+            # MyTensor["dim1 dim2"]
+            if isinstance(params, str):
+                return default_jax_dtype[array_type, params]
+
+            elif isinstance(params, tuple):
+                if len(params) != 2:
+                    raise Exception(f"unexpected type for params:\n{cls.param_info(params)}")
+
+                if isinstance(params[0], str):
+                    # MyTensor["dim1 dim2", int]
+                    return TYPE_TO_JAX_DTYPE[params[1]][array_type, params[0]]
+                
+                elif isinstance(params[0], tuple):
+                    if legacy_mode == "error":
+                        raise Exception(f"legacy mode is set to error, but legacy type was used:\n{cls.param_info(params)}")
+                    elif legacy_mode == "warn":
+                        warnings.warn(f"legacy type annotation was used:\n{cls.param_info(params)}")
+                    # MyTensor[("dim1", "dim2"), int]
+                    shape_anot: list[str] = list()
+                    for x in params[0]:
+                        if isinstance(x, (str, int)):
+                            shape_anot.append(x)
+                        elif isinstance(x, tuple):
+                            shape_anot.append(''.join(str(y) for y in x))
+                        else:
+                            raise Exception(f"unexpected type for params:\n{cls.param_info(params)}")
+                    
+                    return TYPE_TO_JAX_DTYPE[params[1]][array_type, ' '.join(shape_anot)]
             else:
-                return typing._AnnotatedAlias(array_type, params)
+                raise Exception(f"unexpected type for params:\n{cls.param_info(params)}")
 
-    BaseTensor.__name__ = name
+    _BaseArray.__name__ = name
+    _BaseArray.__doc__ = _BaseArray.__doc__.format(
+        default_jax_dtype=repr(default_jax_dtype),
+        array_type=repr(array_type),
+    )
 
-    return BaseTensor
+    return _BaseArray
+
+# this makes linters happy
 
 class ATensor(torch.Tensor):
     @typing._tp_cache
     def __class_getitem__(cls, params):
         raise NotImplementedError()
 
-ATensor = annotated_array_factory(torch.Tensor, "ATensor")
+ATensor = jaxtype_factory("ATensor", torch.Tensor, jaxtyping.Float)
 
-class NDArray(np.ndarray):
+class NDArray(torch.Tensor):
     @typing._tp_cache
     def __class_getitem__(cls, params):
         raise NotImplementedError()
 
-NDArray = annotated_array_factory(np.ndarray, "NDArray")
+NDArray = jaxtype_factory("NDArray", np.ndarray, jaxtyping.Float)
 
 
 

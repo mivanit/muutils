52654ae74e7fcba6547492f08083fb1d26ba636b
mivanit <mivanits@umich.edu>
2023-03-27 15:31:46 -0600

    more tests, split some up

---
+++ tests/test_zanj_sdc_modelcfg.py
@@ -0,0 +1,155 @@
+import json
+from pathlib import Path
+import typing
+
+import pytest
+import numpy as np
+import pandas as pd
+import torch
+from transformers import PreTrainedTokenizer
+
+from muutils.json_serialize import (
+    SerializableDataclass,
+    serializable_dataclass,
+    serializable_field,
+)
+from muutils.zanj import ZANJ
+
+np.random.seed(0)
+
+# pylint: disable=missing-function-docstring,missing-class-docstring
+
+TEST_DATA_PATH: Path = Path("tests/junk_data")
+
+@serializable_dataclass
+class ModelCfg(SerializableDataclass):
+    name: str
+    num_layers: int
+    hidden_size: int
+    dropout: float
+
+
+@serializable_dataclass(kw_only=True)
+class TrainCfg(SerializableDataclass):
+    name: str
+    weight_decay: float
+    optimizer: typing.Type[torch.optim.Optimizer] = serializable_field(
+        default_factory=lambda: torch.optim.Adam,
+        serialization_fn=lambda x: x.__name__,
+        loading_fn=lambda data: getattr(torch.optim, data["optimizer"]),
+    ) 
+    optimizer_kwargs: dict[str, typing.Any] = serializable_field(  # type: ignore
+        default_factory=lambda: dict(lr=0.000001)
+    )
+
+
+class CustomCfg:
+    def __init__(self, x: int, y: str):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def serialize(self):
+        return {"x": self.x, "y": self.y}
+
+    @classmethod
+    def load(cls, data):
+        return cls(
+            **{
+                "x": data["x"],
+                "y": data["y"],
+            }
+        )
+
+
+@serializable_dataclass
+class BasicCfgHolder(SerializableDataclass):
+    model: ModelCfg
+    optimizer: TrainCfg
+    custom: CustomCfg | None = serializable_field(
+        default=None,
+        serialization_fn=lambda x: x.serialize(),
+        loading_fn=lambda data: CustomCfg.load(data["custom"]),
+    )
+
+instance_basic: BasicCfgHolder = BasicCfgHolder(
+    ModelCfg("lstm", 3, 128, 0.1),
+    TrainCfg(
+        name="adamw",
+        weight_decay=0.2,
+        optimizer=torch.optim.AdamW,
+        optimizer_kwargs=dict(lr=0.0001),
+    ),
+    CustomCfg(42, "forty-two"),
+)
+
+
+def test_config_holder():
+    instance_stored = instance_basic.serialize()
+    with open(TEST_DATA_PATH / "test_config_holder.json", "w") as f:
+        json.dump(instance_stored, f, indent="\t")
+    recovered = BasicCfgHolder.load(instance_stored)
+    assert instance_basic == recovered
+
+
+def test_config_holder_zanj():
+    z = ZANJ()
+    path = TEST_DATA_PATH / "test_config_holder.zanj"
+    z.save(instance_basic, path)
+    recovered = z.read(path)
+    assert instance_basic == recovered
+
+
+@serializable_dataclass(kw_only=True)
+class BaseGPTConfig(SerializableDataclass):
+    """
+    Add a name property and serialization to HookedTransformerConfig
+    """
+
+    name: str
+    act_fn: str
+    d_model: int
+    d_head: int
+    n_layers: int
+
+@serializable_dataclass(kw_only=True)
+class AdvCfgHolder(SerializableDataclass):
+    """
+    Handles any logic that moves data between the configs below it.
+    """
+
+    name: str = serializable_field(default="default")
+    model_cfg: BaseGPTConfig
+    tokenizer: CustomCfg | None = serializable_field(
+        default=None,
+        serialization_fn=lambda x: repr(x) if x is not None else None,
+        loading_fn=lambda data: None if data["tokenizer"] is None else NotImplementedError,
+    )
+
+instance_adv: AdvCfgHolder = AdvCfgHolder(
+    model_cfg=BaseGPTConfig(
+        name="gpt2",
+        act_fn="gelu",
+        d_model=128,
+        d_head=64,
+        n_layers=3,
+    ),
+    tokenizer=None,
+)
+
+
+def test_adv_config_holder():
+    instance_stored = instance_adv.serialize()
+    with open(TEST_DATA_PATH / "test_adv_config_holder.json", "w") as f:
+        json.dump(instance_stored, f, indent="\t")
+    recovered = AdvCfgHolder.load(instance_stored)
+    assert instance_adv == recovered
+
+def test_adv_config_holder_zanj():
+    z = ZANJ()
+    path = TEST_DATA_PATH / "test_adv_config_holder.zanj"
+    z.save(instance_adv, path)
+    recovered = z.read(path)
+    assert instance_adv == recovered

17d0b69904c9c5222f3cfd4f2c59dc183760a717
mivanit <mivanits@umich.edu>
2023-04-04 19:42:44 -0600

    basic test of TimerContext

---
+++ tests/logger/test_timer_context.py
@@ -0,0 +1,13 @@
+from muutils.logger.timing import TimerContext
+
+def test_timer_context():
+
+    with TimerContext() as timer:
+        x: float = 1.0
+
+    assert isinstance(timer.start_time, float)
+    assert isinstance(timer.end_time, float)
+    assert isinstance(timer.elapsed_time, float)
+    assert timer.start_time <= timer.end_time
+    assert timer.elapsed_time >= 0.0
+

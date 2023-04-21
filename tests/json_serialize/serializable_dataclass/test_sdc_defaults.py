0e3c161c0bf9918662d7f73af28878ec9d35a840
mivanit <mivanits@umich.edu>
2023-04-21 13:05:21 -0600

    bug uncovered >:(

---
+++ tests/json_serialize/serializable_dataclass/test_sdc_defaults.py
@@ -0,0 +1,51 @@
+import typing
+
+import torch
+
+from muutils.json_serialize import SerializableDataclass, serializable_dataclass, serializable_field
+from muutils.json_serialize.util import JSONitem
+
+# pylint: disable=missing-class-docstring
+
+@serializable_dataclass
+class Config(SerializableDataclass):
+    name: str = serializable_field(default="default_name")
+    save_model: bool = serializable_field(default=True)
+    batch_size: int = serializable_field(default=64)
+
+
+def test_sdc_empty():
+    instance = Config()
+    serialized = instance.serialize()
+    assert serialized == {
+        "name": "default_name",
+        "save_model": True,
+        "batch_size": 64,
+        "__format__": "Config(SerializableDataclass)",
+    }
+    recovered = Config.load(serialized)
+    assert recovered == instance
+
+
+TYPE_MAP: dict[str, type] = {
+    x.__name__: x
+    for x in [int, float, str, bool]
+}
+
+@serializable_dataclass
+class ComplicatedConfig(SerializableDataclass):
+    name: str = serializable_field(default="default_name")
+    save_model: bool = serializable_field(default=True)
+    batch_size: int = serializable_field(default=64)
+
+    some_type: type = serializable_field(
+        default_factory=lambda self: float,
+        serialization_fn=lambda x: x.__name__,
+        loading_fn=lambda data: TYPE_MAP[data["some_type"]],
+    )
+
+def test_sdc_empty_complicated():
+    instance = ComplicatedConfig()
+    serialized = instance.serialize()
+    recovered = ComplicatedConfig.load(serialized)
+    assert recovered == instance

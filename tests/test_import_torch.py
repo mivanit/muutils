aaabe2ffe668fb129742e93563e90a7ab8432150
mivanit <mivanits@umich.edu>
2023-06-13 14:37:44 -0600

    format

--- tests/test_import_torch.py
+++ tests/test_import_torch.py
@@ -1,14 +1,3 @@
-import json
-import os
-import random
-import typing
-import warnings
-from itertools import islice
-from pathlib import Path
-from typing import Any, Callable, TypeVar
-
-import numpy as np
-
 def test_import_torch():
     import torch
 

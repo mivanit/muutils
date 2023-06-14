47970f31da7bebe3b721336805d5b78a1ed65b56
mivanit <mivanits@umich.edu>
2023-06-14 01:02:16 -0600

    dont format the reference scripts generated from notebooks

--- tests/input_data/notebooks_converted/dummy_notebook.py
+++ tests/input_data/notebooks_converted/dummy_notebook.py
@@ -5,31 +5,32 @@
 # ------------------------------------------------------------
 
 
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 # ------------------------------------------------------------
 # Disable matplotlib plots, done during processing by `convert_ipynb_to_script.py`
 import matplotlib.pyplot as plt
-
-# %%
-import numpy as np
-
 plt.show = lambda: None
 # ------------------------------------------------------------
 
 
-# %%
+#%%
 """
 # heading
 
 this is a markdown cell, it should appear in the output as a comment
 """
 
-# %%
+#%%
 print("Hello world!")
 
-# %%
+#%%
 array = np.array([1, 2, 3])
 print(array)
 
-# %%
+#%%
 plt.plot(array)
 plt.show()

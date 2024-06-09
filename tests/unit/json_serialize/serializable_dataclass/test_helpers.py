cccb769b1b86f0b019a76951040842223f96ad94
mivanit <mivanits@umich.edu>
2024-06-09 01:52:49 -0700

    format

--- tests/unit/json_serialize/serializable_dataclass/test_helpers.py
+++ tests/unit/json_serialize/serializable_dataclass/test_helpers.py
@@ -1,10 +1,15 @@
-import numpy as np
-import torch
 import dataclasses
 
+import numpy as np
 import pytest
+import torch
 
-from muutils.json_serialize.serializable_dataclass import serializable_field, array_safe_eq, dc_eq
+from muutils.json_serialize.serializable_dataclass import (
+    array_safe_eq,
+    dc_eq,
+    serializable_field,
+)
+
 
 @dataclasses.dataclass(eq=False)
 class TestClass:
@@ -14,12 +19,13 @@ class TestClass:
     e: list[int] = serializable_field()
     f: dict[str, int] = serializable_field()
 
+
 instance1 = TestClass(
     a=1,
     b=np.array([1, 2, 3]),
     c=torch.tensor([1, 2, 3]),
     e=[1, 2, 3],
-    f={'key1': 1, 'key2': 2}
+    f={"key1": 1, "key2": 2},
 )
 
 instance2 = TestClass(
@@ -27,8 +33,9 @@ instance2 = TestClass(
     b=np.array([1, 2, 3]),
     c=torch.tensor([1, 2, 3]),
     e=[1, 2, 3],
-    f={'key1': 1, 'key2': 2}
+    f={"key1": 1, "key2": 2},
 )
+
 
 def test_array_safe_eq():
     assert array_safe_eq(np.array([1, 2, 3]), np.array([1, 2, 3]))
@@ -36,22 +43,34 @@ def test_array_safe_eq():
     assert array_safe_eq(torch.tensor([1, 2, 3]), torch.tensor([1, 2, 3]))
     assert not array_safe_eq(torch.tensor([1, 2, 3]), torch.tensor([4, 5, 6]))
 
-@pytest.mark.parametrize("instance1, instance2, expected", [
-    (instance1, instance2, True),
-    (instance1, TestClass(
-        a=1,
-        b=np.array([4, 5, 6]),
-        c=torch.tensor([1, 2, 3]),
-        e=[1, 2, 3],
-        f={'key1': 1, 'key2': 2}
-    ), False),
-    (instance1, TestClass(
-        a=2,
-        b=np.array([1, 2, 3]),
-        c=torch.tensor([1, 2, 3]),
-        e=[1, 2, 3],
-        f={'key1': 1, 'key2': 2}
-    ), False)
-])
+
+@pytest.mark.parametrize(
+    "instance1, instance2, expected",
+    [
+        (instance1, instance2, True),
+        (
+            instance1,
+            TestClass(
+                a=1,
+                b=np.array([4, 5, 6]),
+                c=torch.tensor([1, 2, 3]),
+                e=[1, 2, 3],
+                f={"key1": 1, "key2": 2},
+            ),
+            False,
+        ),
+        (
+            instance1,
+            TestClass(
+                a=2,
+                b=np.array([1, 2, 3]),
+                c=torch.tensor([1, 2, 3]),
+                e=[1, 2, 3],
+                f={"key1": 1, "key2": 2},
+            ),
+            False,
+        ),
+    ],
+)
 def test_dc_eq(instance1, instance2, expected):
     assert dc_eq(instance1, instance2) == expected

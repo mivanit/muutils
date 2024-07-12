f4a7c6f4813845ae90ae679c00dfc8ca2f1e826f
mivanit <mivanits@umich.edu>
2024-07-12 00:47:42 -0400

    better NumericSequence type hint?

--- muutils/statcounter.py
+++ muutils/statcounter.py
@@ -8,7 +8,7 @@ from itertools import chain
 from typing import Callable, Optional, Sequence, Union
 
 # _GeneralArray = Union[np.ndarray, "torch.Tensor"]
-NumericSequence = Sequence[Union[float, int]]
+NumericSequence = Sequence[Union[float, int, "NumericSequence"]]
 
 # pylint: disable=abstract-method
 

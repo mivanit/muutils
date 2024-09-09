695e1eae43af8beb7f5f9b62bde5c15998c16ce4
mivanit <mivanits@umich.edu>
2024-09-09 16:57:44 -0600

    fix some type hints, better bytes/str handling in hash funcs

--- muutils/misc/hashing.py
+++ muutils/misc/hashing.py
@@ -4,20 +4,26 @@ import base64
 import hashlib
 
 
-def stable_hash(s: str) -> int:
+def stable_hash(s: str | bytes) -> int:
     """Returns a stable hash of the given string. not cryptographically secure, but stable between runs"""
     # init hash object and update with string
+    s_bytes: bytes
     if isinstance(s, str):
-        s = bytes(s, "UTF-8")
-    hash_obj: hashlib._Hash = hashlib.sha256(s)
+        s_bytes = bytes(s, "UTF-8")
+    else:
+        s_bytes = s
+    hash_obj: hashlib._Hash = hashlib.sha256(s_bytes)
     # get digest and convert to int
     return int.from_bytes(hash_obj.digest(), "big")
 
 
 def base64_hash(s: str | bytes) -> str:
     """Returns a base64 representation of the hash of the given string. not cryptographically secure"""
+    s_bytes: bytes
     if isinstance(s, str):
-        s = bytes(s, "UTF-8")
-    hash_bytes: bytes = hashlib.sha256(s).digest()
+        s_bytes = bytes(s, "UTF-8")
+    else:
+        s_bytes = s
+    hash_bytes: bytes = hashlib.sha256(s_bytes).digest()
     hash_b64: str = base64.b64encode(hash_bytes, altchars=b"-_").decode()
     return hash_b64

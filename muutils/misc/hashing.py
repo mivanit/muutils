2073d7310e389b07f7a291ada25d8d57b0bdf44d
mivanit <mivanits@umich.edu>
2024-09-09 16:52:52 -0600

    add base64 hashing

--- muutils/misc/hashing.py
+++ muutils/misc/hashing.py
@@ -1,12 +1,23 @@
 from __future__ import annotations
 
+import base64
 import hashlib
 
 
 def stable_hash(s: str) -> int:
     """Returns a stable hash of the given string. not cryptographically secure, but stable between runs"""
     # init hash object and update with string
-    hash_obj: hashlib._Hash = hashlib.sha256()
-    hash_obj.update(bytes(s, "UTF-8"))
+    if isinstance(s, str):
+        s = bytes(s, "UTF-8")
+    hash_obj: hashlib._Hash = hashlib.sha256(s)
     # get digest and convert to int
     return int.from_bytes(hash_obj.digest(), "big")
+
+
+def base64_hash(s: str | bytes) -> str:
+    """Returns a base64 representation of the hash of the given string. not cryptographically secure"""
+    if isinstance(s, str):
+        s = bytes(s, "UTF-8")
+    hash_bytes: bytes = hashlib.sha256(s).digest()
+    hash_b64: str = base64.b64encode(hash_bytes, altchars=b"-_").decode()
+    return hash_b64

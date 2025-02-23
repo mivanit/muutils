b7e1055cc9c6f6aa00a632fa1149950413c266a6
mivanit <mivanits@umich.edu>
2025-02-23 02:27:00 -0700

    format

--- muutils/json_serialize/dataclass_transform_mock.py
+++ muutils/json_serialize/dataclass_transform_mock.py
@@ -3,16 +3,14 @@ from __future__ import annotations
 import typing
 from typing import Any, Union
 
+
 def dataclass_transform(
     *,
     eq_default: bool = True,
     order_default: bool = False,
     kw_only_default: bool = False,
     frozen_default: bool = False,
-    field_specifiers: tuple[
-        Union[type[Any], typing.Callable[..., Any]],
-        ...
-    ] = (),
+    field_specifiers: tuple[Union[type[Any], typing.Callable[..., Any]], ...] = (),
     **kwargs: Any,
 ) -> typing.Callable:
     "mock `typing.dataclass_transform` for python <3.11"

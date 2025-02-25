3d8c0601906898c9aa8cf0a912694bb8e214e174
mivanit <mivanits@umich.edu>
2025-02-24 20:17:47 -0700

    b64_decode script

---
+++ muutils/misc/b64_decode.py
@@ -0,0 +1,9 @@
+from sys import argv
+from pathlib import Path
+from base64 import b64decode
+
+if __name__ == "__main__":
+	input_file: Path = Path(argv[1])
+	out: Path = Path(argv[2])
+	input_text: str = input_file.read_text().replace('\n', '')
+	out.write_bytes(b64decode(input_text))

6a912aea2aca0cb73f0b60039faca44d6d74d3ed
Michael Ivanitskiy <mivanits@umich.edu>
2025-05-30 14:23:42 -0600

    wip inlining html stuff

---
+++ muutils/web/inline_html.py
@@ -0,0 +1,136 @@
+from typing import Literal
+from pathlib import Path
+
+
+def inline_html_assets(
+	html: str,
+	assets: list[tuple[Literal["script", "style"], str]],
+	base_path: Path,
+	include_filename_comments: bool = True,
+	prettify: bool = False,
+) -> str:
+	"""Inline specified local CSS/JS files into an HTML document.
+
+	Each entry in `assets` should be a tuple like `("script", "app.js")` or `("style", "style.css")`.
+
+	# Parameters:
+	- `html : str`
+		input HTML content.
+	- `assets : list[tuple[Literal["script", "style"], str]]`
+		List of (tag_type, filename) tuples to inline.
+
+	# Returns:
+	`str` : Modified HTML content with inlined assets.
+	"""
+	for tag_type, filename in assets:
+		if tag_type not in ("style", "script"):
+			err_msg: str = f"Unsupported tag type: {tag_type}"
+			raise ValueError(err_msg)
+
+		# Dynamically create the pattern for the given tag and filename
+		pattern: str
+		if tag_type == "script":
+			pattern = rf'<script src="{filename}"></script>'
+		elif tag_type == "style":
+			pattern = rf'<link rel="stylesheet" href="{filename}">'
+		# assert it's in the text exactly once
+		assert html.count(pattern) == 1, (
+			f"Pattern {pattern} should be in the html exactly once, found {html.count(pattern) = }"
+		)
+		# figure out the indentation level of the pattern  in the html
+		indentation: str = html.split(pattern)[0].splitlines()[-1]
+		assert indentation.strip() == "", (
+			f"Pattern '{pattern}' should be alone in its line, found {indentation = }"
+		)
+		# read the content and create the replacement
+		content: str = (base_path / filename).read_text()
+		# replacement: str = f"<!-- begin '{filename}' -->\n<{tag_type}>\n{content}\n</{tag_type}>\n<!-- end '{filename}' -->"
+		replacement: str = f"<{tag_type}>\n{content}\n</{tag_type}>"
+		if include_filename_comments:
+			replacement = (
+				f"<!-- begin '{filename}' -->\n{replacement}\n<!-- end '{filename}' -->"
+			)
+		# indent the replacement
+		replacement = "\n".join(
+			[f"{indentation}\t{line}" for line in replacement.splitlines()]
+		)
+		# perform the replacement
+		html = html.replace(pattern, replacement)
+
+	if prettify:
+		from bs4 import BeautifulSoup
+
+		soup: BeautifulSoup = BeautifulSoup(html, "html.parser")
+		html = soup.prettify()
+
+	return html
+
+
+def inline_html_file(
+	html_path: Path,
+	output_path: Path,
+	include_filename_comments: bool = True,
+	prettify: bool = False,
+) -> None:
+	base_path: Path = html_path.parent
+	# read the HTML file
+	html: str = html_path.read_text()
+	# read the assets
+	assets: list[tuple[Literal["script", "style"], str]] = []
+	for asset in base_path.glob("*.js"):
+		assets.append(("script", asset.name))
+	for asset in base_path.glob("*.css"):
+		assets.append(("style", asset.name))
+	# inline the assets
+	html_new: str = inline_html_assets(
+		html,
+		assets,
+		base_path,
+		include_filename_comments=include_filename_comments,
+		prettify=prettify,
+	)
+	# write the new HTML file
+	output_path.write_text(html_new)
+
+
+if __name__ == "__main__":
+	import argparse
+
+	parser: argparse.ArgumentParser = argparse.ArgumentParser(
+		description="Inline local CSS/JS files into an HTML document."
+	)
+	parser.add_argument(
+		"-i",
+		"--input-path",
+		type=Path,
+		help="Path to the HTML file to process.",
+	)
+	parser.add_argument(
+		"-o",
+		"--output-path",
+		type=str,
+		help="Path to save the modified HTML file.",
+	)
+
+	parser.add_argument(
+		"-c",
+		"--no-filename-comments",
+		action="store_true",
+		help="don't include comments with the filename in the inlined assets",
+	)
+
+	parser.add_argument(
+		"-p",
+		"--no-prettify",
+		action="store_true",
+		help="don't prettify the HTML file",
+	)
+
+	args: argparse.Namespace = parser.parse_args()
+
+	inline_html_file(
+		html_path=Path(args.input_path),
+		output_path=Path(args.output_path),
+		include_filename_comments=not args.no_filename_comments,
+		prettify=not args.no_prettify,
+	)

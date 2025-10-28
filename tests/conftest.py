42f626d8b3a49f845d46e4e6fac82a373711d957
Michael Ivanitskiy <mivanits@umich.edu>
2025-10-28 14:01:30 +0000

    ignore tests ending in `_torch.py` when in a python version with no torch

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 import sys
 from pathlib import Path
 
-def pytest_ignore_collect(path: Path, config) -> bool:
-	"""ignore any test file ending with `_torch.py` on Python 3.14+"""
-	if sys.version_info < (3, 14):
-		return False
 
-	return str(path).endswith("_torch.py")
+def pytest_ignore_collect(path: Path, config) -> bool:
+    """ignore any test file ending with `_torch.py` on Python 3.14+"""
+    if sys.version_info < (3, 14):
+        return False
+
+    return str(path).endswith("_torch.py")
 
 
 # TODO: get beartype working
